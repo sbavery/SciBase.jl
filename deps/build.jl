baebeeab4b444f1da0d22b1c65ce72d555280ff4
Sam Avery <averysam2@gmail.com>
2019-05-21 10:40:54 -0700

    Create build.jl

---
+++ deps/build.jl
@@ -0,0 +1,25 @@
+using Pkg
+
+if lowercase(get(ENV, "CI", "false")) == "true"
+
+    let basepython = get(ENV, "PYTHON", "python2")
+        envpath = joinpath(@__DIR__, "env")
+        run(`virtualenv --python=$basepython $envpath`)
+
+        if Sys.iswindows()
+            python = joinpath(envpath, "Scripts", "python.exe")
+        else
+            python = joinpath(envpath, "bin", "python2")
+        end
+
+        run(`$python -m pip install numpy`)
+        run(`$python -m pip install scipy`)
+        run(`$python -m pip install matplotlib`)
+        run(`$python -m pip install https://github.com/matplotlib/basemap/archive/v1.0.7rel.tar.gz`)
+        run(`$python -m pip install git+https://github.com/matplotlib/cmocean`)
+        run(`$python -m pip install netcdf4`)
+
+        ENV["PYTHON"] = python
+        Pkg.build("PyCall")
+    end
+end
